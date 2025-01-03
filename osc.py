cebcb0131faa46a3df10477631630d3c6f11d842
Gray Putnam <gputnam@fnal.gov>
2025-01-02 22:52:58 -0600

    Upgrade to 4-neutrino oscillations.

--- osc.py
+++ osc.py
@@ -25,7 +25,11 @@ deltaCP0 = 0.
 
 def oscillate(E, baseline, nuind, nusign, dL=1e2, # m 
               rhoset=None, rhoshift=0, rhoscale=1,
-              th12=th12, th23=th23, th13=th13, dm21=dm21, dm31=dm31, deltaCP=0,
+              # three-neutrino oscillations
+              th12=th12, th23=th23, th13=th13, dm21=dm21, dm31=dm31,
+              # sterile neutrino
+              th14=0, th24=0, th34=0, dm41=0,
+              # anomalous matter potential
               epsee=0, epsemu=0, epsetau=0, epsmumu=0, epsmutau=0, epstautau=0):
 
     if isinstance(E, float) or isinstance(E, int):
@@ -34,23 +38,31 @@ def oscillate(E, baseline, nuind, nusign, dL=1e2, # m
     c12 = np.cos(th12)
     c23 = np.cos(th23)
     c13 = np.cos(th13)
+    c14 = np.cos(th14)
+    c24 = np.cos(th24)
+    c34 = np.cos(th34)
+
     s12 = np.sin(th12)
     s23 = np.sin(th23)
     s13 = np.sin(th13)
+    s14 = np.sin(th14)
+    s24 = np.sin(th24)
+    s34 = np.sin(th34)
     
-    # Vacuum oscillations
-    U = np.array(
-    [
-        [c13*c12, c13*s12, s13*np.exp(-1j*deltaCP)],
-        [-c23*s12-s13*s23*c12*np.exp(1j*deltaCP), c23*c12-s13*s23*s12*np.exp(1j*deltaCP), c13*s23],
-        [s23*s12-s13*c23*c12*np.exp(1j*deltaCP), -s23*c12-s13*c23*s12*np.exp(1j*deltaCP), c13*c23]
+    # Vacuum oscillations -- from https://arxiv.org/pdf/hep-ph/0209097
+    U = np.array([
+	[c12*c13*c14,	c13*c14*s12,	c14*s13,	s14],
+	[-c23*c24*s12 -c12*c24*s13*s23 -c12*c13*s14*s24,	c12*c23*c24 -c24*s12*s13*s23 -c13*s12*s14*s24,	c13*c24*s23 -s13*s14*s24,	c14*s24],
+	[-c12*c23*c34*s13 +c34*s12*s23 -c12*c13*c24*s14*s34 +c23*s12*s24*s34 +c12*s13*s23*s24*s34,	-c23*c34*s12*s13 -c12*c34*s23 -c13*c24*s12*s14*s34 -c12*c23*s24*s34 +s12*s13*s23*s24*s34,	c13*c23*c34 -c24*s13*s14*s34 -c13*s23*s24*s34,	c14*c24*s34],
+	[-c12*c13*c24*c34*s14 +c23*c34*s12*s24 +c12*c34*s13*s23*s24 +c12*c23*s13*s34 -s12*s23*s34,	-c13*c24*c34*s12*s14 -c12*c23*c34*s24 +c34*s12*s13*s23*s24 +c23*s12*s13*s34 +c12*s23*s34,	-c24*c34*s13*s14 -c13*c34*s23*s24 -c13*c23*s34,	c14*c24*c34]
     ])
+
     # Matter Potential
-    # TODO: What is going on with this factor if 2?
-    MP = 2*nusign*np.array([
-        [1+epsee, epsemu, epsetau],
-        [np.conj(epsemu), epsmumu, epsmutau],
-        [np.conj(epsetau), np.conj(epsmutau), epstautau]
+    MP = nusign*np.array([
+        [1-0.5+epsee, epsemu, epsetau, 0],
+        [np.conj(epsemu), -0.5+epsmumu, epsmutau, 0],
+        [np.conj(epsetau), np.conj(epsmutau), -0.5+epstautau, 0],
+        [0, 0, 0, 0]
     ])
 
     if nusign < 0:
@@ -67,4 +79,4 @@ def oscillate(E, baseline, nuind, nusign, dL=1e2, # m
     density += rhoshift
     density *= rhoscale
     
-    return oschelper.do_osc(E, steps, density, U, MP, dm21, dm31, nuind, dL)
+    return oschelper.do_osc(E, steps, density, U, MP, dm21, dm31, dm41, nuind, dL)
--- oschelper/src/oschelper.C
+++ oschelper/src/oschelper.C
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <complex>
 #include <iomanip>
+#include <iostream>
 #include <sstream>
 #include <string>
 
@@ -30,6 +31,8 @@ PyInit_oschelper(void) {
    return PyModule_Create(&cModPyDem);
 }
 
+const static int NNU = 4;
+
 static PyObject *do_osc(PyObject *self, PyObject *args) {
   // inputs
   PyObject *energy_obj;
@@ -39,13 +42,14 @@ static PyObject *do_osc(PyObject *self, PyObject *args) {
   PyObject *MP_obj;
   double dm21;
   double dm31;
+  double dm41;
   int nuind;
   double dL;
 
   // output 
   PyObject *ret = NULL;
 
-  if (!PyArg_ParseTuple(args, "OOOOOddid", &energy_obj, &steps_obj, &density_obj, &U_obj, &MP_obj, &dm21, &dm31, &nuind, &dL)) { 
+  if (!PyArg_ParseTuple(args, "OOOOOdddid", &energy_obj, &steps_obj, &density_obj, &U_obj, &MP_obj, &dm21, &dm31, &dm41, &nuind, &dL)) { 
     PyErr_SetString(PyExc_TypeError, "BAD INPUT TYPES");
     return NULL;
   }
@@ -128,7 +132,7 @@ static PyObject *do_osc(PyObject *self, PyObject *args) {
   int MP_N = MP_dim[0];
   int MP_M = MP_dim[1];
 
-  if (U_N != 3 || U_M != 3 || MP_N != 3 || MP_M != 3) {
+  if (U_N != NNU || U_M != NNU || MP_N != NNU || MP_M != NNU) {
     Py_XDECREF(energy);
     Py_XDECREF(steps);
     Py_XDECREF(density);
@@ -136,22 +140,22 @@ static PyObject *do_osc(PyObject *self, PyObject *args) {
     Py_XDECREF(MP);
 
     std::stringstream error;
-    error << "ERROR BAD MATRIX SIZE, MUST BE 3. U ROW " << U_N << " U COL " << U_M << " MP ROW " << MP_N << " MP COL " << MP_M;
+    error << "ERROR BAD MATRIX SIZE, MUST BE " << NNU << ". U ROW " << U_N << " U COL " << U_M << " MP ROW " << MP_N << " MP COL " << MP_M;
     PyErr_SetString(PyExc_ValueError, error.str().c_str());
 
     return NULL;
   }
 
   // Get local copy of Hamiltonian
-  std::complex<double> Uosc[3][3];
-  for (unsigned i_A = 0; i_A < 3; i_A++) {
-    for (unsigned j_A = 0; j_A < 3; j_A++) {
+  std::complex<double> Uosc[NNU][NNU];
+  for (unsigned i_A = 0; i_A < NNU; i_A++) {
+    for (unsigned j_A = 0; j_A < NNU; j_A++) {
       Uosc[i_A][j_A] = *((std::complex<double>*)PyArray_GETPTR2(U, i_A, j_A));
     }
   }
 
   // zero-initialize output array
-  npy_intp nu_dim[2] = {3, energy_size};
+  npy_intp nu_dim[2] = {NNU, energy_size};
   PyArrayObject *nu = (PyArrayObject *)PyArray_SimpleNew(2, nu_dim, NPY_CDOUBLE);
   if (nu == NULL) {
     Py_XDECREF(energy);
@@ -168,6 +172,7 @@ static PyObject *do_osc(PyObject *self, PyObject *args) {
     *((std::complex<double> *)PyArray_GETPTR2(nu, 0, i)) = 0;
     *((std::complex<double> *)PyArray_GETPTR2(nu, 1, i)) = 0;
     *((std::complex<double> *)PyArray_GETPTR2(nu, 2, i)) = 0;
+    *((std::complex<double> *)PyArray_GETPTR2(nu, 3, i)) = 0;
 
     *((std::complex<double> *)PyArray_GETPTR2(nu, nuind, i)) = 1;
   }
@@ -182,38 +187,47 @@ static PyObject *do_osc(PyObject *self, PyObject *args) {
 
       double E = *((double*)PyArray_GETPTR1(energy, j));
 
-      std::complex<double> A[3][3]; // Build matter potential
-      for (unsigned i_A = 0; i_A < 3; i_A++) {
-        for (unsigned j_A = 0; j_A < 3; j_A++) {
-	  A[i_A][j_A] = 1.52e-4*Ye*rho*E*(*((std::complex<double>*)PyArray_GETPTR2(MP, i_A, j_A)));
+      std::complex<double> A[NNU][NNU]; // Build matter potential
+      for (unsigned i_A = 0; i_A < NNU; i_A++) {
+        for (unsigned j_A = 0; j_A < NNU; j_A++) {
+          // TODO: check factor of 2
+	  A[i_A][j_A] = 2*1.52e-4*Ye*rho*E*(*((std::complex<double>*)PyArray_GETPTR2(MP, i_A, j_A)));
         }
       }
 
       double Losc = dL/hbarc; // m -> 1/eV
       double Eosc = E*1e9; // GeV -> eV
 
-      std::complex<double> thisnu[3] = {*((std::complex<double>*)PyArray_GETPTR2(nu, 0, j)), 
+      std::complex<double> thisnu[NNU] = {*((std::complex<double>*)PyArray_GETPTR2(nu, 0, j)), 
 	                          *((std::complex<double>*)PyArray_GETPTR2(nu, 1, j)),
-                                  *((std::complex<double>*)PyArray_GETPTR2(nu, 2, j))};
+                                  *((std::complex<double>*)PyArray_GETPTR2(nu, 2, j)),
+                                  *((std::complex<double>*)PyArray_GETPTR2(nu, 3, j))};
       std::complex<double> J(0,1);
-      std::complex<double> dnu[3] = {0, 0, 0};
-      dnu[0] = (-J*Losc/(2*Eosc))*(dm31*Uosc[0][2]*std::conj(Uosc[0][2]) + dm21*Uosc[0][1]*std::conj(Uosc[0][1]) + A[0][0])*thisnu[0] +\
-               (-J*Losc/(2*Eosc))*(dm31*Uosc[0][2]*std::conj(Uosc[1][2]) + dm21*Uosc[0][1]*std::conj(Uosc[1][1]) + A[0][1])*thisnu[1] +\
-               (-J*Losc/(2*Eosc))*(dm31*Uosc[0][2]*std::conj(Uosc[2][2]) + dm21*Uosc[0][1]*std::conj(Uosc[2][1]) + A[0][2])*thisnu[2];
+      std::complex<double> dnu[NNU] = {0, 0, 0, 0};
+      dnu[0] = (-J*Losc/(2*Eosc))*(dm31*Uosc[0][2]*std::conj(Uosc[0][2]) + dm21*Uosc[0][1]*std::conj(Uosc[0][1]) + dm41*Uosc[0][3]*std::conj(Uosc[0][3]) + A[0][0])*thisnu[0] +\
+               (-J*Losc/(2*Eosc))*(dm31*Uosc[0][2]*std::conj(Uosc[1][2]) + dm21*Uosc[0][1]*std::conj(Uosc[1][1]) + dm41*Uosc[0][3]*std::conj(Uosc[1][3]) + A[0][1])*thisnu[1] +\
+               (-J*Losc/(2*Eosc))*(dm31*Uosc[0][2]*std::conj(Uosc[2][2]) + dm21*Uosc[0][1]*std::conj(Uosc[2][1]) + dm41*Uosc[0][3]*std::conj(Uosc[2][3]) + A[0][2])*thisnu[2] +\
+               (-J*Losc/(2*Eosc))*(dm31*Uosc[0][2]*std::conj(Uosc[3][2]) + dm21*Uosc[0][1]*std::conj(Uosc[3][1]) + dm41*Uosc[0][3]*std::conj(Uosc[3][3]) + A[0][3])*thisnu[3];
         
-      dnu[1] = (-J*Losc/(2*Eosc))*(dm31*Uosc[1][2]*std::conj(Uosc[0][2]) + dm21*Uosc[1][1]*std::conj(Uosc[0][1]) + A[1][0])*thisnu[0] +\
-               (-J*Losc/(2*Eosc))*(dm31*Uosc[1][2]*std::conj(Uosc[1][2]) + dm21*Uosc[1][1]*std::conj(Uosc[1][1]) + A[1][1])*thisnu[1] +\
-               (-J*Losc/(2*Eosc))*(dm31*Uosc[1][2]*std::conj(Uosc[2][2]) + dm21*Uosc[1][1]*std::conj(Uosc[2][1]) + A[1][2])*thisnu[2];
+      dnu[1] = (-J*Losc/(2*Eosc))*(dm31*Uosc[1][2]*std::conj(Uosc[0][2]) + dm21*Uosc[1][1]*std::conj(Uosc[0][1]) + dm41*Uosc[1][3]*std::conj(Uosc[0][3]) + A[1][0])*thisnu[0] +\
+               (-J*Losc/(2*Eosc))*(dm31*Uosc[1][2]*std::conj(Uosc[1][2]) + dm21*Uosc[1][1]*std::conj(Uosc[1][1]) + dm41*Uosc[1][3]*std::conj(Uosc[1][3]) + A[1][1])*thisnu[1] +\
+               (-J*Losc/(2*Eosc))*(dm31*Uosc[1][2]*std::conj(Uosc[2][2]) + dm21*Uosc[1][1]*std::conj(Uosc[2][1]) + dm41*Uosc[1][3]*std::conj(Uosc[2][3]) + A[1][2])*thisnu[2] +\
+               (-J*Losc/(2*Eosc))*(dm31*Uosc[1][2]*std::conj(Uosc[3][2]) + dm21*Uosc[1][1]*std::conj(Uosc[3][1]) + dm41*Uosc[1][3]*std::conj(Uosc[3][3]) + A[1][3])*thisnu[3];
 
+      dnu[2] = (-J*Losc/(2*Eosc))*(dm31*Uosc[2][2]*std::conj(Uosc[0][2]) + dm21*Uosc[2][1]*std::conj(Uosc[0][1]) + dm41*Uosc[2][3]*std::conj(Uosc[0][3]) + A[2][0])*thisnu[0] +\
+               (-J*Losc/(2*Eosc))*(dm31*Uosc[2][2]*std::conj(Uosc[1][2]) + dm21*Uosc[2][1]*std::conj(Uosc[1][1]) + dm41*Uosc[2][3]*std::conj(Uosc[1][3]) + A[2][1])*thisnu[1] +\
+               (-J*Losc/(2*Eosc))*(dm31*Uosc[2][2]*std::conj(Uosc[2][2]) + dm21*Uosc[2][1]*std::conj(Uosc[2][1]) + dm41*Uosc[2][3]*std::conj(Uosc[2][3]) + A[2][2])*thisnu[2] +\
+               (-J*Losc/(2*Eosc))*(dm31*Uosc[2][2]*std::conj(Uosc[3][2]) + dm21*Uosc[2][1]*std::conj(Uosc[3][1]) + dm41*Uosc[2][3]*std::conj(Uosc[3][3]) + A[2][3])*thisnu[3];
         
-      dnu[2] = (-J*Losc/(2*Eosc))*(dm31*Uosc[2][2]*std::conj(Uosc[0][2]) + dm21*Uosc[2][1]*std::conj(Uosc[0][1]) + A[2][0])*thisnu[0] +\
-               (-J*Losc/(2*Eosc))*(dm31*Uosc[2][2]*std::conj(Uosc[1][2]) + dm21*Uosc[2][1]*std::conj(Uosc[1][1]) + A[2][1])*thisnu[1] +\
-               (-J*Losc/(2*Eosc))*(dm31*Uosc[2][2]*std::conj(Uosc[2][2]) + dm21*Uosc[2][1]*std::conj(Uosc[2][1]) + A[2][2])*thisnu[2];
-
+      dnu[3] = (-J*Losc/(2*Eosc))*(dm31*Uosc[3][2]*std::conj(Uosc[0][2]) + dm21*Uosc[3][1]*std::conj(Uosc[0][1]) + dm41*Uosc[3][3]*std::conj(Uosc[0][3]) + A[3][0])*thisnu[0] +\
+               (-J*Losc/(2*Eosc))*(dm31*Uosc[3][2]*std::conj(Uosc[1][2]) + dm21*Uosc[3][1]*std::conj(Uosc[1][1]) + dm41*Uosc[3][3]*std::conj(Uosc[1][3]) + A[3][1])*thisnu[1] +\
+               (-J*Losc/(2*Eosc))*(dm31*Uosc[3][2]*std::conj(Uosc[2][2]) + dm21*Uosc[3][1]*std::conj(Uosc[2][1]) + dm41*Uosc[3][3]*std::conj(Uosc[2][3]) + A[3][2])*thisnu[2] +\
+               (-J*Losc/(2*Eosc))*(dm31*Uosc[3][2]*std::conj(Uosc[3][2]) + dm21*Uosc[3][1]*std::conj(Uosc[3][1]) + dm41*Uosc[3][3]*std::conj(Uosc[3][3]) + A[3][3])*thisnu[3];
 
       *((std::complex<double>*)PyArray_GETPTR2(nu, 0, j)) += dnu[0];
       *((std::complex<double>*)PyArray_GETPTR2(nu, 1, j)) += dnu[1];
       *((std::complex<double>*)PyArray_GETPTR2(nu, 2, j)) += dnu[2];
+      *((std::complex<double>*)PyArray_GETPTR2(nu, 3, j)) += dnu[3];
 
     }
   }
