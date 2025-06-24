Update baseline parameters. Fix density effect computation in oscillation integrator. Use more sophisticated x-sec model.

check_rate.py
@@ -1,7 +1,7 @@
 from flux import *
 from xsec import *
 
-Etest = 1e3 # GeV
+Etest = 2.5e3 # GeV
 Gtest = Etest / MU_MASS
 
 # Dump
@@ -13,9 +13,9 @@ Nmuon_dump = MU_PER_BUNCH*REPRATE*S2YR*(Lmuontest/C_M_PER_US)/(MU_TAU*Gtest)
 
 # Accelerator
 
-# 30cm straight section from https://arxiv.org/abs/2407.12450, pointed
-# at the telescope 1% of the time (making that up)
-Lmuontest = 0.3*0.01 # m
+# 30cm straight section from https://arxiv.org/abs/2407.12450, 
+# spacing causes 50x reduction from intrinsic (0.02 mrad) to spaced (1 mrad)
+Lmuontest = 0.3*0.02 # m
 
 Nmuon_accelerator = MU_PER_BUNCH*(1e6*C_M_PER_US/CIRCUMFERENCE)*S2YR*(Lmuontest/C_M_PER_US)/(MU_TAU*Gtest)
 
@@ -23,12 +23,14 @@ print("Number of muons [e12]:", Nmuon_dump/1e12, Nmuon_accelerator/1e12)
 
 # Example volume, from P ONE
 # https://pos.sissa.it/444/1175/pdf
-VOLUME = 1e3*np.pi*(5e2)**2
+# VOLUME = 1e3*np.pi*(5e2)**2
+# base volume - 1Gt
+VOLUME = 1e9
 DENSITY = 1 # t / m^3
 
 intrate = xsec(Etest)*DENSITY*np.power(VOLUME, 1./3.)
 
 print("Number of interactions [1e6] / yr:", Nmuon_dump*intrate/1e6, Nmuon_accelerator*intrate/1e6)
 print("Number of interactions / s:", Nmuon_dump*intrate/S2YR, Nmuon_accelerator*intrate/S2YR)
-
 print("Number of interactions / spill:", Nmuon_dump*intrate/(REPRATE*S2YR), Nmuon_accelerator*intrate/((1e6*C_M_PER_US/CIRCUMFERENCE)*S2YR))
+print("Number of interactions / 1e6 muons:", intrate*1e6)

osc.py
@@ -26,7 +26,7 @@ deltaCP0 = 0.
 def oscillate(E, baseline, nuind, nusign, dL=1e2, # m 
               rhoset=None, rhoshift=0, rhoscale=1,
               # three-neutrino oscillations
-              th12=th12, th23=th23, th13=th13, dm21=dm21, dm31=dm31,
+              th12=th12, th23=th23, th13=th13, dm21=dm21, dm31=dm31, deltaCP=deltaCP0,
               # sterile neutrino
               th14=0, th24=0, th34=0, dm41=0,
               # anomalous matter potential
@@ -55,6 +55,13 @@ def oscillate(E, baseline, nuind, nusign, dL=1e2, # m
 	[-c23*c24*s12 -c12*c24*s13*s23 -c12*c13*s14*s24,	c12*c23*c24 -c24*s12*s13*s23 -c13*s12*s14*s24,	c13*c24*s23 -s13*s14*s24,	c14*s24],
 	[-c12*c23*c34*s13 +c34*s12*s23 -c12*c13*c24*s14*s34 +c23*s12*s24*s34 +c12*s13*s23*s24*s34,	-c23*c34*s12*s13 -c12*c34*s23 -c13*c24*s12*s14*s34 -c12*c23*s24*s34 +s12*s13*s23*s24*s34,	c13*c23*c34 -c24*s13*s14*s34 -c13*s23*s24*s34,	c14*c24*s34],
 	[-c12*c13*c24*c34*s14 +c23*c34*s12*s24 +c12*c34*s13*s23*s24 +c12*c23*s13*s34 -s12*s23*s34,	-c13*c24*c34*s12*s14 -c12*c23*c34*s24 +c34*s12*s13*s23*s24 +c23*s12*s13*s34 +c12*s23*s34,	-c24*c34*s13*s14 -c13*c34*s23*s24 -c13*c23*s34,	c14*c24*c34]
+    ]).astype(np.complex128)
+
+    U += np.array([
+        [0, 0, s13*np.exp(-1.0j*deltaCP), 0],
+        [-c12*s23*s13*np.exp(1.0j*deltaCP), -s12*s23*s13*np.exp(1.0j*deltaCP), 0, 0],
+        [-c12*c23*s13*np.exp(1.0j*deltaCP), -s12*c23*s13*np.exp(1.0j*deltaCP), 0, 0],
+        [0, 0, 0, 0]
     ])
 
     # Matter Potential

xsec.py
@@ -1,5 +1,31 @@
-def xsec(Enu):
+import numpy as np
+
+def xsec_simple(Enu):
     return 4e-13*Enu # m2/t, Enu in GeV
 
-def xsecbar(Enu):
+def xsecbar_simple(Enu):
     return 2e-13*Enu # m2/t, Enu in GeV
+
+def xsec(Enu):
+    dat = np.loadtxt("nuOxsec_pernucleon_1e-38.txt", delimiter=",")
+    # nucleons per tonne
+    n_nucleon_per_tonne = 6.022*1e23*1e6
+    scale = 1e-38*1e-4*n_nucleon_per_tonne
+    return np.interp(Enu, dat[0], Enu*dat[1]*scale)
+
+def xsecbar(Enu):
+    dat = np.loadtxt("nubarOxsec_pernucleon_1e-38.txt", delimiter=",")
+    # nucleons per tonne
+    n_nucleon_per_tonne = 6.022*1e23*1e6
+    scale = 1e-38*1e-4*n_nucleon_per_tonne
+    return np.interp(Enu, dat[0], Enu*dat[1]*scale)
+
+if __name__ == "__main__":
+    print("Xsec check")
+
+    print("Compare nu at 1 TeV", xsec(1e3), xsec_simple(1e3))
+    print("Compare nubar at 1 TeV", xsecbar(1e3), xsecbar_simple(1e3))
+
+    print("Compare nu at 5 TeV", xsec(5e3), xsec_simple(5e3))
+    print("Compare nubar at 5 TeV", xsecbar(5e3), xsecbar_simple(5e3))
+